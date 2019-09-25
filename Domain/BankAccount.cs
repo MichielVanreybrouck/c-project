278bc06ee76157f258bbc9d4a62d5dd5c82ceb17
Michiel Vanreybrouck <michiel.vanreybrouck@student.hogent.be>
2019-09-25 10:03:34 +0200

    Initial check-in of module Banking

---
+++ Domain/BankAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace Banking.Domain
+{
+    public class BankAccount
+    {
+        #region fields and properties
+
+        private decimal _balance;
+
+        public decimal Balance {
+            get {
+                return _balance;
+            }
+            private set {
+                if (value < 0)
+                    throw new ArgumentException("Balance can not be negative");
+                _balance = value;
+            }
+        }
+
+        public string AccountNumber { get; } = "123-456789-99";
+
+
+
+
+        public BankAccount(string acountNumber,  decimal  intitialBalance = 0)
+        {
+            AccountNumber = acountNumber;
+            Balance = intitialBalance;
+            //Cost = 3;
+            //costReadOnly = 3;
+        }
+
+        #region
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Amount cannot be negative");
+            }
+            else
+            {
+                Balance += amount;
+            }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Amount cannot be negative");
+            }
+            else
+            {
+                Balance -= amount;
+            }
+        }
+
+
+
+   
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Banking.Domain;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount("123-456789-99");
+            BankAccount ba2 = new BankAccount("000-456789-99", 1000);
+
+            Console.WriteLine($"Acount number is {ba.AccountNumber} - Balance is {ba.Balance}");
+            Console.WriteLine($"Acount number is {ba2.AccountNumber} - Balance is {ba2.Balance}");
+        }
+    }
+}
